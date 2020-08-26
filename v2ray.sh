896b73d67a1cab2f5ae6d1f392022ed074cc0a85
yeahwu <yeahwu@users.noreply.github.com>
2020-08-26 22:13:26 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -2,30 +2,64 @@
 
 # config v2ray
 cat << EOF > /usr/bin/v2ray/config.json
-{		
-    "inbounds": 		
-    [		
-        {		
-            "port": $PORT,"protocol": "vmess",		
-            "settings": {"clients": [{"id": "$UUID"}]},		
-            "streamSettings": {"network": "ws"}		
-        }		
-    ],		
-    "outbounds": 		
-    [		
-        {"protocol": "freedom"},		
-        {"protocol": "blackhole","tag": "blocked"},
-        {"protocol": "socks","tag": "socksTor","settings": {"servers": [{"address": "127.0.0.1","port": 9050}]}}		
-    ],		
-    		
-    "routing": 		
-    {		
-        "rules": 		
-        [		
-            {"type": "field","outboundTag": "socksTor","domain": ["geosite:tor"]},		
-            {"type": "field","outboundTag": "blocked","domain": ["geosite:category-ads-all"]}		
-        ]		
-    }		
+{
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        },
+        {
+            "protocol": "blackhole",
+            "tag": "blocked"
+        },
+        {
+            "protocol": "socks",
+            "tag": "sockstor",
+            "settings": {
+                "servers": [
+                    {
+                        "address": "127.0.0.1",
+                        "port": 9050
+                    }
+                ]
+            }
+        }
+    ],
+
+    "routing": {
+        "rules": [
+            {
+                "type": "field",
+                "outboundTag": "sockstor",
+                "domain": [
+                    "geosite:tor"
+                ]
+            },
+            {
+                "type": "field",
+                "outboundTag": "blocked",
+                "domain": [
+                    "geosite:category-ads-all"
+                ]
+            }
+        ]
+    }
 }		
 EOF
 
